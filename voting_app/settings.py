5dfd80182ef4718a8102abe3f8377c5e46fdc923
Shred258 <billymillitha03@yahoo.com>
2025-05-18 21:32:28 +0300

    Fixed database configuration in Render

--- voting_app/settings.py
+++ voting_app/settings.py
@@ -80,21 +80,22 @@ WSGI_APPLICATION = 'voting_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/voting_app',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+
 
 # For local SQLite fallback (development only)
-if os.environ.get('DATABASE_URL') is None:
-    DATABASES['default'] = {
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
