18aafd2e695ca8d0a06b4e9684edbb78eb993610
Shred258 <billymillitha03@yahoo.com>
2025-05-18 21:46:57 +0300

    Configure static files for Render

--- voting_app/settings.py
+++ voting_app/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
